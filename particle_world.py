4a0dbb982b9a2817b2926698934ad1c043285803
John Francis <john9francis@gmail.com>
2024-03-20 15:08:58 -0600

    Added a function to enforce wrap-around boundary conditions

--- particle_world.py
+++ particle_world.py
@@ -33,6 +33,40 @@ class ParticleWorld:
         else:
           self.lattice = np.vstack((self.lattice, np.array([i*a1 + j*a2])))
 
+    self.enforce_periodic_boundary_conditions(self.lattice, [a1, a2], self.x_size, self.y_size)
+    
+
+
+  def enforce_periodic_boundary_conditions(self, 
+  positions_array, 
+  lattice_vectors, 
+  x_max, 
+  y_max,
+  x_min=0, 
+  y_min=0):
+    '''
+    If a particle's position is beyond the x_max, it shifts it back x_max points. 
+    NOTE: This is dangerous, because if there are too many particles to fit in 
+    the box constrained by x_max and y_max, the particles that fall off the edges
+    will go on top of some other particles! It is necessary that somewhere else in code
+    we define correctly the size of the box and the number of particles. 
+    Perhaps a function that takes in a number of particles and calculates a box that will
+    fit them, or vice versa. 
+    '''
+    for i in range(len(positions_array)):
+      pos_x = positions_array[i][0]
+      pos_y = positions_array[i][1]
+
+      if pos_x > x_max:
+        pos_x -= x_max * lattice_vectors[0][0]
+      if pos_y > y_max:
+        pos_y -= y_max * lattice_vectors[1][1]
+
+      positions_array[i] = np.array([pos_x, pos_y])
+
+
+    return positions_array
+
 
 
   def plot_lattice(self):
