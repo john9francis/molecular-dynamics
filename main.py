6bc45d9cdfd9037f3bd6e4b6a70a8a9916dd5198
John Francis <john9francis@gmail.com>
2024-03-29 13:02:10 -0600

    Made the main file the pygame main file.

--- main.py
+++ main.py
@@ -1,16 +1,56 @@
+# a main file that runs an animation with pygame
+
+import time
+import pygame
 from particle_world import ParticleWorld
-import numpy as np
 
 def main():
-  p = ParticleWorld()
-  box_size = 5
 
-  # RECREATE TABLE:
-  positions = np.array([[4,4],[3.5,1.5],[0.5,2]])
+  # set up our own stuff
+  p_world = ParticleWorld(36)
+  #p_world.plot_lattice()
 
-  print("Location:     Net force:")
-  for i in range(len(positions)):
-    print(f"{positions[i]}, {p.calculate_net_force(positions, i, box_size)}")
+  # Initialize Pygame
+  pygame.init()
+
+  # Set up the screen
+  screen_width = 800
+  screen_height = 500
+  screen = pygame.display.set_mode((screen_width, screen_height))
+  pygame.display.set_caption("Molecular Dynamics")
+
+  # Set up colors
+  BLACK = (0, 0, 0)
+  WHITE = (255, 255, 255)
+
+
+  # Main game loop
+  running = True
+  while running:
+    # Event handling
+    for event in pygame.event.get():
+      if event.type == pygame.QUIT:
+        running = False
+
+    # Clear the screen
+    screen.fill(BLACK)
+
+    # draw the particles NOTE: y=0 is at the top of the screen, not the bottom
+    scale_x = screen_width / p_world.width
+    scale_y = screen_height / p_world.height
+
+    for p in p_world.lattice:
+      pygame.draw.rect(screen, WHITE, (p[0] * scale_x, p[1] * scale_y, 10, 10))
+
+    p_world.update()
+    time.sleep(.01)
+
+    # Update the display
+    pygame.display.flip()
+
+  # Quit Pygame
+  pygame.quit()
+
 
 if __name__ == "__main__":
   main()
